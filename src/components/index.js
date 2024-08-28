c7145976b14c9fb442474953778b3f0933dc6ca0
Mashookh Khan <mashooq.ahmed175@gmail.com>
2024-08-29 00:31:38 +0530

    signup change

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,7 @@ import Container from "./container/Container"
 import Logo from "./Logo"
 import LogoutBtn from "./Header/LogoutBtn"
 import RTE from "./RTE"
-import SignUp from './Signup'
+import SignUp from './SignUp'
 import Login from './Login'
 import PostForm from './PostForm'
 import PostCard from "./PostCard"
